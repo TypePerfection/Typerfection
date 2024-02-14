00b344cc89933b86b33a3c0dcde893b455777f5f
Ethan Chalupa <eachalupa@gmail.com>
2024-02-13 21:28:37 -0700

    fixes

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 function Header() {
     const links: { title: string; url: string; }[] = [
@@ -10,15 +11,15 @@ function Header() {
 
 
     const linkList = links.map(link =>
-        <div className="hover:bg-slate-50 h-full flex w-20 text-white hover:text-violet-600">
-            <a className="m-auto  text-xl font-mono" href={link.url}>{link.title}</a>
+        <div key={link.title} className="hover:bg-slate-50 h-full flex w-20 text-white hover:text-violet-600">
+            <Link className="m-auto  text-xl font-mono" href={link.url}>{link.title}</Link>
         </div>
     )
     return (
         <nav className='flex w-full bg-violet-600 top-0 sticky h-10 '>
-            <a href="/">
+            <Link href="/">
                 <img src={image} className="h-10"></img>
-            </a>
+            </Link>
 
             {linkList}
         </nav>
