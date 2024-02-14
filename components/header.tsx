9aeb315a64d9484a993178a3ae8357d2dba91923
Ethan Chalupa <cha21093@byui.edu>
2024-02-13 19:48:25 -0500

    Added idea for header

---
+++ components/header.tsx
@@ -0,0 +1,8 @@
+export default function Header(){
+
+    return (
+        <div className='fixed h-60 mb-60 w-full bg-slate-400'>
+            <a href="google.com">Text</a>
+        </div>
+        )
+}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
- 
+import Header from '@/components/header'
+
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <header>
+        <Header/>
+      </header>
       <body>
         <Main />
         <NextScript />
