7645cae0c831ed164ee623b5bb20bd729ffa3c17
Ethan Chalupa <cha21093@byui.edu>
2024-03-11 14:12:03 -0400

    removed filler link

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,18 +5,17 @@ function NavBar() {
     const links: { title: string; url: string; }[] = [
         { title: "Posts", url: "posts" },
         { title: "Components", url: "compShowcase" },
-        { title: "BYU-I", url: "https://www.BYUI.edu" },
         { title: "App", url: "app" },
     ]
     const image = "/keyboard.svg";
 
     const linkList = links.map(link =>
-        <div key={link.title} className="hover:bg-slate-50 h-full flex w-20 text-white hover:text-violet-600">
+        <div key={link.title} className="hover:bg-slate-50 h-full flex text-white hover:text-violet-600">
             <Link className="m-auto text-xl font-mono" href={link.url}>{link.title}</Link>
         </div>
     )
     return (
-        <nav className='flex w-full bg-violet-600 top-0 sticky h-10 '>
+        <nav className='flex w-full bg-violet-600 top-0 sticky h-10 gap-2'>
             <Link href="/">
                 <img src={image} className="h-10"></img>
             </Link>
