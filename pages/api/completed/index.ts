2d09d2d9c4f704945ad7e2db161425e2585e7fa7
Ethan Chalupa <eachalupa@gmail.com>
2024-04-02 02:17:54 -0600

    ts more like trash

--- pages/api/completed/index.ts
+++ pages/api/completed/index.ts
@@ -3,7 +3,7 @@ import { getSession } from 'next-auth/react';
 import prisma from '../../../lib/prisma';
 
 // POST /api/post
-export default async function handle(req, res) {
+export default async function handle(req: any, res: any) {
   const { textId, userEmail, accuracy, wpm } = req.body;
   const result = await prisma.completedTestingText.create({
     data: {
