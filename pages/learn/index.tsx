35def6d3b4026a62a090fe61b70b527a81fbce04
Ethan Chalupa <cha21093@byui.edu>
2024-04-02 16:42:07 -0400

    css fix

--- pages/learn/index.tsx
+++ pages/learn/index.tsx
@@ -41,11 +41,11 @@ const Blog: React.FC<Props> = (levels) => {
     let output = levels.feed.map((level) => (
         <div className="border-t mx-2" key={level.id}>
             <div>{level.name}</div>
-            <div className="flex gap-2">
+            <div className="flex gap-2 mb-2">
                 {level.texts.map((text) => (
                     <div key={text.id} className="post">
                         <div onClick={() => Router.push("/app/[id]", `/app/${text.id}`)}>
-                            <div className="bg-gray-300 p-2 w-20 rounded-md border border-gray-400 text-center mb-2">
+                            <div className="bg-gray-300 p-2 w-20 rounded-md border border-gray-400 text-center">
                                 Stage {stupidCounter++}{/* I am the dumbest programmer ever */}
                             </div>
                         </div>
